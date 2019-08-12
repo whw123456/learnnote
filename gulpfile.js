e7bdb9eb328c407bce7cc1329b2823161281ed6c
wanghongwei <wanghongwei01@koolearn-inc.com>
2019-08-12 14:02:55 +0800

    去除html压缩

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp"),
     fileinclude = require("gulp-file-include"), //模板中间件
     plumber=require('gulp-plumber'),//检测错误
     del=require('del'),
+    htmlmin = require('gulp-htmlmin'),//html压缩
     nodemon=require('gulp-nodemon'),
     gutil=require('gulp-util');//如果有自定义方法，会用到
 
@@ -20,6 +21,16 @@ gulp.task("cleanPaper", function(cb) {
 
 // html整合
 gulp.task("htmlTemplate", function() {
+    var options = {
+        removeComments: false,//清除HTML注释
+        collapseWhitespace: false,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
     return gulp
         .src(['workSpace/**/*.html','!workSpace/template','!workSpace/template/*'])
         .pipe(plumber({ errorHandler: errorHandler }))
@@ -29,6 +40,7 @@ gulp.task("htmlTemplate", function() {
                 cssFile: "", //引用文件路径
             })
         )
+        // .pipe(htmlmin(options))
         .pipe(gulp.dest("public/"));
 });
 
